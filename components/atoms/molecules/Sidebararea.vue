4ce6bcef061af8ed36fcf65a3055eacd6091225f
y.izumisawa <y-izumisawa@visual-w.co.jp>
2025-03-31 10:28:14 +0900

    サイドバーエリアを作成

--- components/atoms/molecules/Sidebararea.vue
+++ components/atoms/molecules/Sidebararea.vue
@@ -1,7 +1,12 @@
-<template>
-  test
-</template>
-
 <script setup>
   defineProps({})
 </script>
+
+<template>
+  <ul>
+    <li>Dashboard</li>
+    <li>MocPage</li>
+    <li>Parts</li>
+    <li>Colors</li>
+  </ul>
+</template>
