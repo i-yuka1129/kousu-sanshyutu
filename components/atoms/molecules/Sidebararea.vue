a1bd0f2c61469d98aa9fac531ea4e72d6537fcf0
y.izumisawa <y-izumisawa@visual-w.co.jp>
2025-05-07 11:47:12 +0900

    【コンポーネント】サイドバーエリア メニューのアイコン追加

--- components/atoms/molecules/Sidebararea.vue
+++ components/atoms/molecules/Sidebararea.vue
@@ -4,10 +4,22 @@
 
 <template>
   <ul class="p-4 bg-[#D0E4E6]">
-    <li class="p-2">Dashboard</li>
-    <li class="p-2">MocPage</li>
-    <li class="p-2">Parts</li>
-    <li class="p-2">Colors</li>
+    <li class="p-2 flex items-center gap-1">
+      <UIcon name="i-heroicons-squares-2x2" class="w-5 h-5" />
+      Dashboard
+    </li>
+    <li class="p-2 flex items-center gap-1">
+      <UIcon name="i-heroicons-computer-desktop" class="w-5 h-5" />
+      MocPage
+    </li>
+    <li class="p-2 flex items-center gap-1">
+      <UIcon name="i-heroicons-wrench" class="w-5 h-5" />
+      Parts
+    </li>
+    <li class="p-2 flex items-center gap-1">
+      <UIcon name="i-heroicons-eye-dropper" class="w-5 h-5" />
+      Colors
+    </li>
   </ul>
 </template>
 
