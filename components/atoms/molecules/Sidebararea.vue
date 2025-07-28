af202dba5290862e24b53451eb1b0fda251d9870
y.izumisawa <y-izumisawa@visual-w.co.jp>
2025-07-28 11:25:58 +0900

    【コンポーネント】サイドバーにボタンを追加

--- components/atoms/molecules/Sidebararea.vue
+++ components/atoms/molecules/Sidebararea.vue
@@ -5,10 +5,14 @@
 <template>
   <ul class="p-4 bg-[#D0E4E6]">
     <li class="p-2 flex items-center gap-1">
-      <UIcon name="i-heroicons-cursor-arrow-ripple-solid" class="w-8 h-8" />
+      <button>
+        <UIcon name="i-heroicons-cursor-arrow-ripple-solid" class="w-8 h-8" />
+      </button>
     </li>
     <li class="p-2 flex items-center gap-1">
-      <UIcon name="i-heroicons-squares-plus" class="w-8 h-8" />
+      <button>
+        <UIcon name="i-heroicons-squares-plus" class="w-8 h-8" />
+      </button>
     </li>
   </ul>
 </template>
